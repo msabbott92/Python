55de916200ee40c126cdc4e2b4d66bc0f90aa14d
Matthew Abbott <123894644+msabbott92@users.noreply.github.com>
2023-06-19 08:35:29 -0600

    Store Products exercise

--- fundamentals/extras/Store and Products/StoreProducts.py
+++ fundamentals/extras/Store and Products/StoreProducts.py
@@ -1,8 +1,8 @@
 class Store:
-    def __init__(self, name, type):
+    def __init__(self, name, type, products):
         self.name = name
         self.type = type
-        self.products = Product(name, type, price)
+        self.products = products
 
     def add_product(self, newproduct):
         self.products.append(newproduct)
@@ -10,6 +10,9 @@ class Store:
 
     def sell_product(self,id):
         self.products.remove(self.products[id])
+
+    def printInventory(self):
+        print(str(self.products))
         
         
 
@@ -33,12 +36,14 @@ class Product:
 
 
 product1 = Product("Redington Rod", "Fly Rod", 200)
+product2 = Product("Orvis Rod","Fly rod", 250 )
 
 product1.printInfo()
-mainStore = Store("Matt's Fly Shop", "Outdoor Retailer")
+mainStore = Store("Matt's Fly Shop", "Outdoor Retailer", [product1, product2])
 
 product1.updated_price(0.1, True)
 product1.printInfo()
 
 mainStore.add_product(product1).add_product(product1)
 mainStore.sell_product(0)
+mainStore.printInventory()
