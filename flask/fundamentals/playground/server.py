2f12be419656d26b2989a013d196005c04104e32
Matthew Abbott <123894644+msabbott92@users.noreply.github.com>
2023-06-24 15:55:40 -0600

    playground flask

--- flask/fundamentals/playground/server.py
+++ flask/fundamentals/playground/server.py
@@ -3,12 +3,15 @@ app = Flask(__name__)
 
 @app.route("/play")
 def level_one():
-    return render_template("index.html")
+    return render_template("index.html", num=3, color= "blue")
 
-@app.route("/play/<int:num>/<string:color>")
-def level_two(num, color):
-    return render_template("level2.html", num=num, color=color)
+@app.route("/play/<int:num>")
+def level_two(num):
+    return render_template("index.html", num=num, color= "blue")
 
+@app.route("/play/<int:num>/<color>")
+def level_three(num, color):
+    return render_template("index.html", num=num, color=color)
 
 if __name__=="__main__":
     app.run(debug=True)
