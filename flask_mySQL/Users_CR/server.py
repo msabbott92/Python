4d0407b078d6a6d65213f8eb273d5ab58aae894a
Matthew Abbott <123894644+msabbott92@users.noreply.github.com>
2023-07-02 10:31:02 -0600

    Users MVC

--- flask_mySQL/Users_CR/server.py
+++ flask_mySQL/Users_CR/server.py
@@ -1,48 +1,10 @@
-from flask import Flask, render_template, redirect, request
-from users import User
+from flask_app import app
+from flask_app.controllers import users
+
+
+from flask_app.models import user
 app = Flask(__name__)
 
-
-@app.route("/")
-def index():
-    return render_template("index.html", users=User.get_all())
-
-@app.route('/create')
-def new_user():
-    return render_template("create.html")
-
-@app.route('/create_user', methods=["POST"])
-def new_create():
-    print(request.form)
-    User.save(request.form)
-    return redirect("/")
-
-@app.route("/users/<int:id>")
-def show_user(id):
-    data = {
-        "id": id
-    }
-    return render_template("user.html", user=User.get_one(data))
-
-@app.route("/edit/<int:id>")
-def edit(id):
-    data = {
-        "id":id
-    }
-    return render_template("edit.html",user=User.get_one(data))
-
-@app.route('/update',methods=['POST'])
-def update():
-    User.update(request.form)
-    return redirect('/')
-
-@app.route('/delete/<int:id>')
-def destroy(id):
-    data = {
-        "id":id
-    }
-    User.destroy(data)
-    return redirect('/')
 
 
 if __name__ == "__main__":
